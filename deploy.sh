f2620171f4720cbc78d1eb3a18ab9852f17c2c2e
Gregg Keary <gregg@gkeary.com>
2016-07-11 08:05:33 -0400

    setup shell scripts

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+cp ./scenicflyer.lua "${HOME}/Desktop/X-Plane 10/Resources/plugins/FlyWithLua/Scripts/scenicflyer.lua"
+
+
---
+++ rename.sh
@@ -0,0 +1,3 @@
+mv "${HOME}/Desktop/X-Plane 10/Resources/plugins/FlyWithLua/Scripts/scenicflyer.lua" "${HOME}/Desktop/X-Plane 10/Resources/plugins/FlyWithLua/Scripts/scenicflyer.xlua"
+
+
